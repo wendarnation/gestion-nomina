5c4b888e438c15be15344724a063aa4687dd3bb9
Carlos Cortés Candela <ccorcan398@g.educaand.es>
2024-09-26 11:59:12 +0200

    Actualizacion clase Nomina

--- src/main/java/es/iessoterhernandez/daw/dwes/GestionNomina/Laboral/Nomina.java
+++ src/main/java/es/iessoterhernandez/daw/dwes/GestionNomina/Laboral/Nomina.java
@@ -4,6 +4,8 @@ public class Nomina {
 	
 	private static final int SUELDO_BASE[] = {50000, 70000, 90000, 110000, 130000, 150000, 170000, 190000, 210000, 230000};
 	
+
+	// Calcula noómina tenienedo en cuenta categoría y años trabajados
 	public double sueldo(Empleado emp) {
 		
 		int categoriaSueldo = emp.getCategoria();
