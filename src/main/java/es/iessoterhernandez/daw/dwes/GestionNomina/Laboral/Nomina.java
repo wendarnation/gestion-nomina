9f2d0fe388c3ca6f9270d8d60b5d3819e2ffa4e5
Carlos Cortés Candela <ccorcan398@g.educaand.es>
2024-09-27 13:29:57 +0200

    Añadido Javadoc

--- src/main/java/es/iessoterhernandez/daw/dwes/GestionNomina/Laboral/Nomina.java
+++ src/main/java/es/iessoterhernandez/daw/dwes/GestionNomina/Laboral/Nomina.java
@@ -5,7 +5,7 @@ public class Nomina {
 	private static final int SUELDO_BASE[] = {50000, 70000, 90000, 110000, 130000, 150000, 170000, 190000, 210000, 230000};
 	
 
-	// Calcula nómina tenienedo en cuenta categoría y años trabajados
+	// Calcula nómina teniendo en cuenta categoría y años trabajados
 	public double sueldo(Empleado emp) {
 		
 		int categoriaSueldo = emp.getCategoria();
